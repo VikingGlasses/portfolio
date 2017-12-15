c3334e9fd65b587f3a690aac88efb6afcd284684
Benjamin Rosman <rosman.benji@gmail.com>
2017-12-15 16:41:54 +0100

    TrainingSession method javadoc

--- src/org/benjamin/portfolio/model/bean/TrainingSession.java
+++ src/org/benjamin/portfolio/model/bean/TrainingSession.java
@@ -9,20 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(scopeName="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
 public class TrainingSession {
-	
-	// 
 
+	/**
+	 * Compile / summaries the data into a list of doubles.
+	 * @return The compiled data.
+	 */
 	public List<Double> summariesData() {
 		// TODO Implement training session summaries data
 		// generate data list from variables
 		return null;
 	}
 
+	/**
+	 * Handles new data input.
+	 * @param name 
+	 */
 	public void handle(String name) {
 		// TODO Implement training session handle
 		
 	}
-
-	// Data collection
 	
 }
