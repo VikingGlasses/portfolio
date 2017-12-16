2e784cd0ec64a81699e1fc28a2b0c5b023da3915
Benjamin Rosman <rosman.benji@gmail.com>
2017-12-16 17:51:00 +0100

    AdminController javadoc

--- src/org/benjamin/portfolio/model/controller/AdminController.java
+++ src/org/benjamin/portfolio/model/controller/AdminController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Administration API with CRUD functionality over Codes.
+ * 
+ * @author Benjamin Rosman
+ *
+ */
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -19,7 +25,16 @@ public class AdminController {
 	@Autowired
 	private CodeRepository codeRepository;
 	
-	// create Code
+	/**
+	 * Creates and persistently stores a new code.
+	 * 
+	 * @param code The code.
+	 * @param train The training behavior to be associated with this code.
+	 * @param predict The prediction behavior to be associated with this code.
+	 * @param view The view behavior to be associated with this code.
+	 * @param admin If this code should give administrative privilege.
+	 * @return The code object.
+	 */
 	@RequestMapping(path = "/create/{code}/{train}/{predict}/{view}/{admin}", 
 					method = RequestMethod.POST,
 					produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -35,7 +50,11 @@ public class AdminController {
 		return codeRepository.save(objCode);
 	}
 	
-	// Read Codes
+	/**
+	 * Returns all codes.
+	 * 
+	 * @return List with all codes.
+	 */
 	@RequestMapping(path = "/codes", 
 					method = RequestMethod.GET,
 					produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -46,7 +65,16 @@ public class AdminController {
 		return list;
 	}
 
-	// update Code
+	/**
+	 * Updates or creates a new code if doesn't exist.
+	 * 
+	 * @param code The code.
+	 * @param train The training behavior to be associated with this code.
+	 * @param predict The prediction behavior to be associated with this code.
+	 * @param view The view behavior to be associated with this code.
+	 * @param admin If this code should give administrative privilege.
+	 * @return The code object.
+	 */
 	@RequestMapping(path = "/update/{code}/{train}/{predict}/{view}/{admin}", 
 					method = RequestMethod.POST,
 					produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -67,7 +95,11 @@ public class AdminController {
 		return codeRepository.save(objCode);
 	}
 
-	// delete Code
+	/**
+	 * Deletes the code.
+	 * 
+	 * @param code Code to be deleted.
+	 */
 	@RequestMapping(path = "/delete/{code}", 
 					method = RequestMethod.POST,
 					produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
