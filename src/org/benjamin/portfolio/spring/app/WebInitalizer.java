b1873c4fc8ed12c91a517015d0177875fb51764e
Benjamin Rosman <rosman.benji@gmail.com>
2017-12-16 12:53:15 +0100

    WebInitalizer javadoc + cleaning

--- src/org/benjamin/portfolio/spring/app/WebInitalizer.java
+++ src/org/benjamin/portfolio/spring/app/WebInitalizer.java
@@ -2,7 +2,6 @@ package org.benjamin.portfolio.spring.app;
 
 import java.util.Collection;
 
-import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -11,19 +10,25 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+/**
+ * Configuration of the ServletContext.
+ * 
+ * @author Benjamin Rosman
+ * @see WebApplicationInitializer
+ *
+ */
 public class WebInitalizer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-		context.register(WebConfig.class);
-		context.register(MongodbConfig.class);
+		context.register(WebConfig.class, MongodbConfig.class);
 		
 		ServletRegistration.Dynamic dispatcher = servletContext
 								.addServlet("dispatcher", new DispatcherServlet(context));
 		int LOAD_ON_START = 1;
 		dispatcher.setLoadOnStartup(LOAD_ON_START);
-		dispatcher.addMapping("/*");
+		dispatcher.addMapping("/");
 		Collection<String> mappings = dispatcher.getMappings();
 		mappings.stream().forEach(string -> System.out.println(string));
 		
