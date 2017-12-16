3a7873511ea65a8a6704a70a4c5139d9278ff2b1
Benjamin Rosman <rosman.benji@gmail.com>
2017-12-16 15:02:20 +0100

    TrainingSession javadoc

--- src/org/benjamin/portfolio/model/bean/TrainingSession.java
+++ src/org/benjamin/portfolio/model/bean/TrainingSession.java
@@ -6,27 +6,35 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
+/**
+ * Session bean for handling new user input and compiling it into usable numeric data,
+ * for storage and machine learning purposes.
+ * 
+ * @author Benjamin Rosman
+ *
+ */
 @Component
 @Scope(scopeName="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
 public class TrainingSession {
 
 	/**
-	 * Compile / summaries the data into a list of doubles.
-	 * @return The compiled data.
+	 * Compile / summaries the containing data into a list of doubles that is usable for 
+	 * saving and machine learning calculations.
+	 * @return The compiled numeric data as a list.
 	 */
 	public List<Double> summariesData() {
 		// TODO Implement training session summaries data
-		// generate data list from variables
+		// generate data list from content
 		return null;
 	}
 
 	/**
-	 * Handles new data input.
-	 * @param name 
+	 * Handles new data input that can be compiled to usable numeric data.
+	 * @param input New input data.
 	 */
-	public void handle(String name) {
+	public void handle(String input) {
 		// TODO Implement training session handle
-		
+		// update content.
 	}
 	
 }
