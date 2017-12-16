8b4c61be76f9cb1cd7a716e3b324953ef6675613
Benjamin Rosman <rosman.benji@gmail.com>
2017-12-16 13:33:59 +0100

    TrainingRepository code consitency

--- src/org/benjamin/portfolio/model/repository/TrainingRepository.java
+++ src/org/benjamin/portfolio/model/repository/TrainingRepository.java
@@ -2,9 +2,9 @@ package org.benjamin.portfolio.model.repository;
 
 import org.benjamin.portfolio.model.bean.Training;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.RepositoryDefinition;
 
-@Repository
+@RepositoryDefinition(domainClass=Training.class, idClass = String.class)
 public interface TrainingRepository extends MongoRepository<Training, String> {
 
 }
